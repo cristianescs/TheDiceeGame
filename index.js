219a30513f17c0dab0bb3e170237b9a450c51742
Cristiane Silva <cristiane.silvac@hotmail.com>
2022-03-22 19:29:58 -0300

    Solved

--- index.js
+++ index.js
@@ -7,4 +7,12 @@
     var dicePlayer2 = "dice" + randomNumber2;
     document.querySelectorAll("img")[1].setAttribute("src", "./img/" + dicePlayer2 + ".png");
 
-    
+    var winner = document.getElementById("winner");
+
+    if (randomNumber1 > randomNumber2){
+        winner.innerHTML =  "Player 1 wins!";
+    } else if (randomNumber1 < randomNumber2){
+        winner.innerHTML =  "Player 2 wins!";
+    } else {
+        winner.innerHTML = "Draw!";
+    }
